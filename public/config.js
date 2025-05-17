d5d3396c503bb40e99916ae360dcef0b0fdc593f
Christopher Sembrano <christophersembrano@wvsu.edu.ph>
2025-05-17 22:49:55 +0000

    modified:   public/config.js

--- public/config.js
+++ public/config.js
@@ -1,12 +1,4 @@
-// config.js
+// public/config.js
 const config = {
-    // Get the current hostname and protocol
-    getBaseUrl: function() {
-        // If we're on localhost, use localhost:3000
-        if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
-            return 'http://localhost:3000';
-        }
-        // Otherwise, use the current origin
-        return window.location.origin;
-    }
-}; 
+  getBaseUrl: () => "https://fictional-bassoon-g47p6pqx75gx29rwj-3000.app.github.dev"
+};
